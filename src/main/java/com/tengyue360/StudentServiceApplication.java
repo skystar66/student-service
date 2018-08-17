c4248c51c1fe3c49322101769b25c4a10f9cae0e
xuliang <xuliang@tengyue360.com>
2018-08-17 18:12:29 +0800

    fixed 耗时优化

--- src/main/java/com/tengyue360/StudentServiceApplication.java
+++ src/main/java/com/tengyue360/StudentServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,8 @@ import javax.servlet.http.HttpServletResponse;
 @EnableDiscoveryClient
 @MapperScan("com.tengyue360.dao")
 @RestController
+@ServletComponentScan
+//@ImportResource(locations = { "classpath:druid-bean.xml" })
 public class StudentServiceApplication {
 
 
