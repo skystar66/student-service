4e607ea9a143dde5bf8221cefc897a87d2996131
xuliang <xuliang@tengyue360.com>
2018-08-18 16:33:09 +0800

    #028 学生个人模块 优化

--- src/main/java/com/tengyue360/web/BeanValidators/BeanValidators.java
+++ src/main/java/com/tengyue360/web/BeanValidators/BeanValidators.java
@@ -209,7 +209,10 @@ public class BeanValidators {
         } else if (StringUtils.isBlank(model.getNewPwd())) {
             //新密码不能不能为空
             return new ResponseResult(ReturnCode.PASSWORD_EMPTY.code(), ReturnCode.PASSWORD_EMPTY.msg(), null);
-        } else if (StringUtils.isNotBlank(model.getMessageCode())) {
+        } else if (StringUtils.isBlank(model.getUserId())) {
+            //新密码不能不能为空
+            return new ResponseResult(ReturnCode.ERROR_USER_ID.code(), ReturnCode.ERROR_USER_ID.msg(), null);
+        }else if (StringUtils.isNotBlank(model.getMessageCode())) {
             String cacheCode = redisTemplate.opsForValue().get(ValidateCodeEnum.FORGET_PWD_CODE.getKey() + model.getPhone()) == null ? ""
                     : redisTemplate.opsForValue().get(ValidateCodeEnum.FORGET_PWD_CODE.getKey() + model.getPhone()).toString();
             if (!cacheCode.equals(model.getMessageCode())) {
