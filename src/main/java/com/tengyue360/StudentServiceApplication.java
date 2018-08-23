7a781db22c565ba371c7a490dd416fb48510db14
xuliang <xuliang@tengyue360.com>
2018-08-23 14:59:33 +0800

    fix jsonObject

--- src/main/java/com/tengyue360/StudentServiceApplication.java
+++ src/main/java/com/tengyue360/StudentServiceApplication.java
@@ -1,14 +1,19 @@
 package com.tengyue360;
 
+import com.gexin.fastjson.support.config.FastJsonConfig;
+import com.gexin.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.tengyue360.utils.JsonRpcUtils;
 import com.tengyue360.web.requestModel.LoginRequestModel;
 import com.tengyue360.web.responseModel.ResponseResult;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +38,22 @@ public class StudentServiceApplication {
 
         System.out.println("结果："+rwesult);
     }
+
+
+    /**
+     * 使用fastjson做为json的解析器
+     * @return
+     */
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        //	fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+
+
 
 }
